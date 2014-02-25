3a1f4e78a78af84c6be2ca65358d22227653d7af
Karanbir Singh <kbsingh@karan.org>
2014-02-25 19:09:35 +0000

    just run the entire thing for t_xen.sh

--- t_xen.sh
+++ t_xen.sh
@@ -4,19 +4,34 @@
 # the test suite
 
 TestHost=$1
-RemoteCmd="ssh -t ${TestHost} "
+RemoteCmd="ssh -t ${TestHost} -l root"
 # make sure we can login to ${TestHost}
 
 # setup some baseline stuff
-${RemoteCmd} "yum -y install curl dnsmasq bridge-utils ntpdate openssh-clients"
+${RemoteCmd} "yum -y install curl dnsmasq bridge-utils ntpdate openssh-clients rsync"
 ${RemoteCmd} "ntpdate 0.centos.pool.ntp.org"
 ${RemoteCmd} "yum -y upgrade"
-cat configs/install-xen.cron  | ${RemoteCmd} "crontab -u root -e"
-
+${RemoteCmd} "yum -y install centos-release-xen"
+${RemoteCmd} "yum -y install xen"
+${RemoteCmd} "/usr/bin/grub-bootxen.sh"
 ${RemoteCmd} "reboot"
 
-# wait for the machine to come back
+# wait for the machine to come back, we are going to need a timeout for this...
+# ... also need to handle the case of machine didnt come back at all
+sleep 60
+flag=2
+while [ $flag -gt 1 ]; do
+	ping -c2 ${TestHost} > /dev/null 2>&1
+	if [ $? -eq 0 ]; then
+		flag=0
+		sleep 10
+	fi
+done
 
-# scp the entire set of files from this dir to there 
+# rsync the entire testsuite up
+${RemoteCmd} "mkdir ~/t_xen"
+rsync --exclude =".git/*" -PHvar * root@${TestHost}:t_xen/
 
-${RemoteCmd} "reboot"
+# run it
+
+${RemoteCmd} "cd t_xen; ./runtests.sh"
